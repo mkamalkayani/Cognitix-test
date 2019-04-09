84137c1113272a5d73bf4ca92ed261f2d967e5f3
Muhammad <mkamalkayani@gmail.com>
2019-04-09 18:27:38 +0500

    feat: added Albums list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { UsersListComponent } from "./users-list/users-list.component";
+import { AlbumsListComponent } from './albums-list/albums-list.component';
 
 @NgModule({
-  declarations: [AppComponent, UsersListComponent],
+  declarations: [AppComponent, UsersListComponent, AlbumsListComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
