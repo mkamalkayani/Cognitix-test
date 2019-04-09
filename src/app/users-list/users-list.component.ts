3bf675c4bbd5c6cac5c7aea1869dd81e2406cd0a
Muhammad <mkamalkayani@gmail.com>
2019-04-09 07:20:29 +0500

    add user service to user list component

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from "./../Types/User";
+import { UserService } from "./../user.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-users-list',
-  templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.css']
+  selector: "app-users-list",
+  templateUrl: "./users-list.component.html",
+  styleUrls: ["./users-list.component.css"]
 })
 export class UsersListComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
+    this.userService.getUsers().subscribe(data => (this.users = data));
+    console.log(this.users);
   }
-
 }
