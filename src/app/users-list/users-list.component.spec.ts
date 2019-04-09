4cc47a470ee32ba3318a692a101f44ff221438ac
Muhammad <mkamalkayani@gmail.com>
2019-04-09 17:03:16 +0500

    feat: unit test for users list component

--- src/app/users-list/users-list.component.spec.ts
+++ src/app/users-list/users-list.component.spec.ts
@@ -1,16 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from "@angular/common/http";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { UsersListComponent } from './users-list.component';
+import { UsersListComponent } from "./users-list.component";
 
-describe('UsersListComponent', () => {
+describe("UsersListComponent", () => {
   let component: UsersListComponent;
   let fixture: ComponentFixture<UsersListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UsersListComponent ]
-    })
-    .compileComponents();
+      declarations: [UsersListComponent],
+      imports: [HttpClientModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +20,11 @@ describe('UsersListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should have property users", () => {
+    expect(component.users).toBeUndefined();
   });
 });
