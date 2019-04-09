100d8a9f5c450940aa278dd901a3615f8dfcc28e
Muhammad <mkamalkayani@gmail.com>
2019-04-09 23:18:28 +0500

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { UsersListComponent } from "./users-list/users-list.component";
 import { AlbumsListComponent } from './albums-list/albums-list.component';
+import { PhotosListComponent } from './photos-list/photos-list.component';
 
 @NgModule({
-  declarations: [AppComponent, UsersListComponent, AlbumsListComponent],
+  declarations: [AppComponent, UsersListComponent, AlbumsListComponent, PhotosListComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
