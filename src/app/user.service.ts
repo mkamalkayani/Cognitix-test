67c1baaac4f4646e4bb9dfbacf03ef7639512886
Muhammad <mkamalkayani@gmail.com>
2019-04-09 07:19:55 +0500

    feat: fetch real users from api

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,14 +1,17 @@
 import { User } from "./Types/User";
 import { MockUsers } from "./mock-users";
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class UserService {
-  constructor() {}
+  private usersUrl = "http://jsonplaceholder.typicode.com/users";
+  constructor(private http: HttpClient) {}
 
-  getUsers(): User[] {
-    return MockUsers;
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl);
   }
 }
